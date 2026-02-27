deeeaa7389cd0bbd5b8adf36ee2bc7ec316ddb3f
hbmarkofficial-business <hbmark.official@gmail.com>
2026-02-27 19:46:41 +0100

    Update HeroSection.tsx

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -40,8 +40,8 @@ export const HeroSection = () => {
             className="font-display text-5xl md:text-7xl lg:text-8xl font-bold text-foreground leading-tight opacity-0 animate-fade-up"
             style={{ animationDelay: '400ms', animationFillMode: 'forwards' }}
           >
-            Top
-            <span className="block text-gradient">Salon</span>
+            YP Barbershop 
+            <span className="block text-gradient">Herren und Kinderfriseur</span>
           </h1>
 
           {/* Description */}
